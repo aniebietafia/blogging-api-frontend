1c42ce10cca9652e9d21197badf121401b2c5ac6
aniebietafia <aniebietafia87@gmail.com>
2023-10-21 19:22:01 +0100

    vue app: added script and template

--- blogging-api/src/App.vue
+++ blogging-api/src/App.vue
@@ -1,85 +1,35 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    }
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue)
+      this.enteredValue = ''
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="left">
+    <h1>Welcome to Olive Blogging API</h1>
+    <p>...bring your creative ideas to life.</p>
+  </div>
+  <div class="right">
+    <div>
+      <label for="goal">Goal</label>
+      <input type="text" id="goal" v-model="enteredValue" />
+      <button v-on:click="addGoal">Add Goal</button>
     </div>
-  </header>
-
-  <RouterView />
+    <div>
+      <ul>
+        <li v-for="goal in goals" v-bind:key="goal">{{ goal }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
