63c6dbe208cf3a508c2f66db346d03d33eddfa0d
aniebietafia <aniebietafia87@gmail.com>
2023-10-22 20:33:28 +0100

    generate password: added method to generate strong password

--- blogging-api/src/App.vue
+++ blogging-api/src/App.vue
@@ -3,13 +3,44 @@ export default {
   data() {
     return {
       goals: [],
-      enteredValue: ''
+      enteredValue: '',
+      password: ''
     }
   },
   methods: {
     addGoal() {
       this.goals.push(this.enteredValue)
       this.enteredValue = ''
+    },
+
+    genrateStrongPassword() {
+      const chars =
+        '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+~`|}{[]:;?><,./-='
+      const passwordLength = 16
+      let password = ''
+
+      for (let i = 0; i <= passwordLength; i++) {
+        const randomNumber = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomNumber, randomNumber + 1)
+      }
+
+      this.password = password
+    },
+
+    changePasswordVisibility() {
+      const pwd = document.getElementById('pwd')
+      const confirm = document.getElementById('confirm')
+      const button = document.getElementById('changePwd')
+
+      if (pwd.type === 'password' && confirm.type === 'password') {
+        pwd.type = 'text'
+        confirm.type = 'text'
+        button.textContent = 'HIDE PASSWORD'
+      } else {
+        pwd.type = 'password'
+        confirm.type = 'password'
+        button.textContent = 'SHOW PASSWORD'
+      }
     }
   }
 }
@@ -30,6 +61,16 @@ export default {
       <ul>
         <li v-for="goal in goals" v-bind:key="goal">{{ goal }}</li>
       </ul>
+    </div>
+
+    <div>
+      <label for="pwd">Password</label>
+      <input type="password" id="pwd" v-model="password" />
+      <button id="changePwd" v-on:click="changePasswordVisibility">SHOW PASSWORD</button>
+
+      <label for="confirm">Confirm Password</label>
+      <input type="password" id="confirm" v-model="password" />
+      <button v-on:click="genrateStrongPassword">Generate Password</button>
     </div>
   </div>
 </template>
