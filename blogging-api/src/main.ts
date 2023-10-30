e844e35175ca428ef9ecbfbcc248bee6b02cc8a9
aniebietafia <aniebietafia87@gmail.com>
2023-10-30 18:00:04 +0100

    refactored boilerplate: removed default boilerplate

--- blogging-api/src/main.ts
+++ blogging-api/src/main.ts
@@ -4,11 +4,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import SignInPage from './components/SignInPage.vue'
+
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('sign-in', SignInPage)
 
 app.mount('#app')
